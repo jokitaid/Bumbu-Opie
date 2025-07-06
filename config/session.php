70889800bb685a4339971ee1c842540b2efe8f11
ABDUL RAHMAN <rahman787887@gmail.com>
2025-07-06 18:30:29 +0700

    update script sessions

--- config/session.php
+++ config/session.php
@@ -171,6 +171,7 @@ return [
 
     // 'secure' => env('SESSION_SECURE_COOKIE'),
     'secure' => env('SESSION_SECURE_COOKIE', true),
+    'same_site' => 'lax',
 
     /*
     |--------------------------------------------------------------------------
